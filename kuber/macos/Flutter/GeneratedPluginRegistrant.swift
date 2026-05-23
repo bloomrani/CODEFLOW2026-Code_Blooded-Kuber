7f4b54fa640e42ffa93d5d01a6d9392e50606fff
Ritankar Bose <ritankarbose100@gmail.com>
2026-05-23 22:17:09 +0530

    Register Firebase plugins for desktop
    Add registration for firebase_auth and firebase_core in macOS and Windows generated plugin registrants, adjusting import/registration order accordingly. Also update the generated_plugins.cmake plugin list to include share_plus and url_launcher after firebase entries. These changes align desktop plugin registration with newly added Firebase dependencies.

--- kuber/macos/Flutter/GeneratedPluginRegistrant.swift
+++ kuber/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,18 +6,15 @@ import FlutterMacOS
 import Foundation
 
 import file_picker
+import firebase_auth
+import firebase_core
 import share_plus
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
-  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-import firebase_auth
-import firebase_core
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
